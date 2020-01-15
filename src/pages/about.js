9408831fde5341abb732d41bd43e4fe0771555e4
GAURAV SAHA <gauravsaha385@gmail.com>
2020-01-15 23:58:25 +0530

    Initial Upload

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => 
+    
+        <div>
+            <h1>About Gatsby Sample App</h1>
+         <p>This site is made for absolute begineers for demonstration purpose.</p>
+
+        </div>
+    
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to Gatsby Sample App</h1>
+    <p>This is a sample site for beginners!!</p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
